2838469e9d7053c4d74e0c3f566d933c9ec0a14b
abdaliDahir <abdali.dahir@gmail.com>
2021-01-02 21:47:31 +0100

    add server logic

--- MERN-PROJECT/server/index.js
+++ MERN-PROJECT/server/index.js
@@ -1,5 +1,24 @@
 import express from 'express';
-import cors from 'cors';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
+import postRoutes from './routes/rouotes';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+// Atlas cloud 
+const CONNECTION_URL = 'mongodb+srv://user:pass@practice.jto9p.mongodb.net/test';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
